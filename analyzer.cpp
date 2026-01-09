36796246ff4afba61508b653c2aee13dc72e9286
lamar-tech19 <lamaran.balshi1@bahcesehir.edu.tr>
2026-01-09 20:22:29 +0300

    Update analyzer.cpp

--- analyzer.cpp
+++ analyzer.cpp
@@ -1,26 +1,137 @@
 #include "analyzer.h"
 
-// Students may use ANY data structure internally
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+// Internal storage
+unordered_map<string, long long> zoneCounts;
+unordered_map<string, long long> zoneHourCounts;
 
 void TripAnalyzer::ingestFile(const std::string& csvPath) {
-    // TODO:
-    // - open file
-    // - skip header
-    // - skip malformed rows
-    // - extract PickupZoneID and pickup hour
-    // - aggregate counts
+    ifstream file(csvPath);
+
+    if (!file.is_open()) {
+        cout << "File cannot be opened" << endl;
+        return;
+    }
+    
+    string line;
+
+    // Skipping the header
+    getline(file, line);
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string field;
+        vector<string> fields;
+
+        while (getline(ss, field, ',')) {
+            fields.push_back(field);
+        }
+
+        // We expect exactly 6 columns
+        if (fields.size() != 6) {
+            continue;
+        }
+
+        string pickupZone = fields[1];
+        string dateTime   = fields[3];
+
+        // Find space between date and time
+        int spacePos = dateTime.find(' ');
+        if (spacePos == -1) {
+            continue;
+        }
+
+        // Take hour (HH)
+        string hourStr = dateTime.substr(spacePos + 1, 2);
+
+        int hour = stoi(hourStr);
+
+        if (hour > 23) {
+            continue;
+        }
+
+        // Counting the zone
+        zoneCounts[pickupZone]++;
+
+        // Counting the zone + hour
+        string key = pickupZone + "#" + to_string(hour);
+        zoneHourCounts[key]++;
+    }
+
+    file.close();
 }
 
-std::vector<ZoneCount> TripAnalyzer::topZones(int k) const {
-    // TODO:
-    // - sort by count desc, zone asc
-    // - return first k
-    return {};
+// Sorting zones
+bool compareZones(ZoneCount a, ZoneCount s) {
+    if (a.count == s.count) {
+        return a.zone < s.zone;
+    }
+    return a.count > s.count;
 }
 
-std::vector<SlotCount> TripAnalyzer::topBusySlots(int k) const {
-    // TODO:
-    // - sort by count desc, zone asc, hour asc
-    // - return first k
-    return {};
+vector<ZoneCount> TripAnalyzer::topZones(int k) const {
+    vector<ZoneCount> output;
+
+    for (auto it = zoneCounts.begin(); it != zoneCounts.end(); ++it) {
+        ZoneCount x;
+        x.zone = it->first;
+        x.count = it->second;
+        output.push_back(x);
+    }
+
+    sort(output.begin(), output.end(), compareZones);
+
+    if (output.size() > k) {
+        output.resize(k);
+    }
+
+    return output;
+}
+
+// Sorting slots 
+bool compareSlots(SlotCount a, SlotCount s) {
+    if (a.count == s.count) {
+        if (a.zone == s.zone) {
+        return a.hour < s.hour;
+    }
+        return a.zone < s.zone;
+    }
+    return a.count > s.count;
+}
+
+vector<SlotCount> TripAnalyzer::topBusySlots(int k) const {
+    vector<SlotCount> output;
+
+    for (auto it = zoneHourCounts.begin(); it != zoneHourCounts.end(); ++it) {
+        string key = it->first;
+        long long cnt = it->second;
+
+        int spacePos = key.find('#');
+        if (spacePos == -1) {
+            continue;
+        }
+
+        SlotCount s;
+        s.zone = key.substr(0, spacePos);
+        s.hour = stoi(key.substr(spacePos + 1));
+        s.count = cnt;
+
+        output.push_back(s);
+    }
+
+    sort(output.begin(), output.end(), compareSlots);
+
+    if (output.size() > k) {
+        output.resize(k);
+    }
+
+    return output;
 }
